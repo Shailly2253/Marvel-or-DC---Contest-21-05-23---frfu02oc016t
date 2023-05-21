76cf3c1190d8347a4f12a71d8a5054277bafb994
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-05-21 20:29:32 +0530

    Code added

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,15 +1,16 @@
-import { useState } from "react";
+import React, { useState } from "react";
 function Summary({ formData }) {
     return (
         <div id="summary">
             <h2>Summary</h2>
-            <p>Form Type: </p>
-            <p>Age: </p>
+            <p>Form Type: {formData.type}</p>
+            <p>Age: {formData.age}</p>
 
-            <p>DC Shows: </p>
-
-            <p>Marvel Shows: </p>
-
+            {formData.type === "FormA" ? (
+                <p>DC Shows: {formData.show}</p>
+            ) : (
+                <p>Marvel Shows: {formData.show}</p>
+            )}
         </div>
     );
 }
