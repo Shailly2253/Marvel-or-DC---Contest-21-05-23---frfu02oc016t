4eed369a281ff215ec639665e9206b446215d480
CodewithShailly <121444727+Shailly2253@users.noreply.github.com>
2023-08-27 18:15:01 +0530

    Update FormB.js

--- src/components/FormB.js
+++ src/components/FormB.js
@@ -1,26 +1,18 @@
-import React, { useState, useEffect } from "react";
-import Summary from "./Summary";
+import { useState, useEffect } from "react";
 function FormB({ onSubmit, age }) {
     const [marvelShows, setMarvelShows] = useState("");
-    const [formData, setFormData] = useState("");
 
     const handleSubmit = (event) => {
-        event.preventDefault();
-        if (marvelShows != "") {
-            setFormData({
-                type: "Form B",
-                age: age,
-                show: marvelShows,
-            });
-        }
     };
 
-    return formData === "" ? (
-        <form id="marvel" onSubmit={handleSubmit}>
+
+
+    return (
+        <form id="marvel" >
             <h2>Form B</h2>
             <label>
                 Select Marvel Shows:
-                <select onChange={(e) => setMarvelShows(e.target.value)}>
+                <select  >
                     <option value="">--Select--</option>
                     <option value="WandaVision">WandaVision</option>
                     <option value="The Falcon and the Winter Soldier">
@@ -36,12 +28,8 @@ function FormB({ onSubmit, age }) {
                 <input type="number" value={age} disabled />
             </label>
             <br />
-            <button id="submit-marvel" type="submit">
-                Submit
-            </button>
+            <button id="submit-marvel" type="submit">Submit</button>
         </form>
-    ) : (
-        <Summary formData={formData} />
     );
 }
 export default FormB;
